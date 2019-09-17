dd4131545641a9a9c8dc8fb8c2e11035cf933250
aiiksveryown <iihediohaa@gmail.com>
2019-09-17 22:09:36 +0100

    adding user login verification

--- index.js
+++ index.js
@@ -10,8 +10,24 @@ $(function(){
       $(".name2").text(data[1]["firstname"]+" "+data[1]["lastname"])
       $(".manifesto1").text(data[0]["manifesto"])
       $(".manifesto2").text(data[1]["manifesto"])
+  })
+  localStorage.setItem("id", "1")
+  var voterid = localStorage.getItem("id")
+  // console.log(parseInt(voterid))
+  $("#login").click(function() {
+    var lastname = $("#lastname").val()
+    console.log(lastname)
+    var voterid = $("#voterid").val()
+    console.log("click")
+    $.ajax({
+      url: "http://localhost:3000/voters",
+      type: "POST",
+      dataType: "json",
+      contentType: "application/json"
+    }).done(function(data) {
+      ans = data.filter(element => element["lastname"] === lastname && element["id"] === voterid)
+      console.log(ans.length>0)
+    })
+  })
 })
-$(".vote1").onclick(function(params) {
-  
-})
-})
+
