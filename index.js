f2e7f158aeaee57eaa5b685cbda8ef9c7ee25d85
aiiksveryown <iihediohaa@gmail.com>
2019-09-17 14:54:56 +0100

    added candidate population functionality to voters dash

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
 $(function(){
+  $.ajax({
+    url: "http://localhost:3000/candidates",
+    data: {
+    },
+    type: "GET",
+    dataType : "json",
+  }).done(function(data) {
+      $(".name1").text(data[0]["firstname"]+" "+data[0]["lastname"])
+      $(".name2").text(data[1]["firstname"]+" "+data[1]["lastname"])
+      $(".manifesto1").text(data[0]["manifesto"])
+      $(".manifesto2").text(data[1]["manifesto"])
+})
+$(".vote1").onclick(function(params) {
   
+})
 })
