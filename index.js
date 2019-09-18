81c64a018ad145a35ce4b5195642d3c7407e8fd3
aiiksveryown <iihediohaa@gmail.com>
2019-09-18 17:05:12 +0100

    Login/signup modal is functional

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 $(function(){
+
+  // Populate Candidate Details in user dash
   $.ajax({
     url: "http://localhost:3000/candidates",
     data: {
@@ -11,15 +13,18 @@ $(function(){
       $(".manifesto1").text(data[0]["manifesto"])
       $(".manifesto2").text(data[1]["manifesto"])
   })
-  console.log(localStorage.getItem("id"))
+
+  // On click login, if user logged in before then go to dashboard
   $(".navlogin").click(function(){
     if (localStorage.getItem("id")!==null) {
       document.location=dash.html
     }
   })
+
+  // Validate login and add user id to local storage
   $("#login2").click(function() {
-    var lastname = $("#lastname").val()
-    var voterid = $("#voterid").val()
+    var ninlogin = $("#ninlogin").val()
+    var passwordlogin = $("#passwordlogin").val()
     error = true
     $.ajax({
       url: "http://localhost:3000/voters",
@@ -29,8 +34,9 @@ $(function(){
       contentType: "application/json"
     }).done(function(data) {
       $.each(data, function(key, value) {
-        if (lastname==value.lastname && voterid==value.id) {
+        if (ninlogin==value.nin && passwordlogin==value.password) {
           error = false
+          voterid = value.id
           let votecheck = value.hasVoted
         }
       })
@@ -49,10 +55,38 @@ $(function(){
       }
     })
   })
+
+  // Populates db with new user details from signup
+  $("#signup").click(function(){
+    var firstnameSignup = $("#firstname").val()
+    var lastnameSignup = $("#lastname").val()
+    var emailSignup = $("#email").val()
+    var passwordSignup = $("#password").val()
+    var ninSignup = $("#nin").val()
+    alreadyExists = false
+    newUser = {
+      "firstname": firstnameSignup, "lastname": lastnameSignup, "email": emailSignup, "password": passwordSignup, "hasVoted": false, "nin": ninSignup
+    }
+
+    $.ajax({
+      url: "http://localhost:3000/voters",
+      type: "POST",
+      data: JSON.stringify(newUser),
+      dataType: "json",
+      contentType: "application/json"
+    }).done(function() {
+      alert("Registration Successful")
+      // document.location="#"
+    })
+  })
+
+  // Clear local storage on user log out
   $("#logout").click(function(){
     localStorage.removeItem("id");
     document.location="/index.html"
   })
+
+  // When user votes, disable the user's ability to vote again
   $(".vote").click(function(){
     votecheck = {"hasVoted": true}
     $.ajax({
@@ -63,6 +97,8 @@ $(function(){
       contentType: "application/json"
     }).done(alert("Thank you for participating!"))
   })
+
+  // When user votes, update candidate1 vote count
   $(".vote1").click(function() {
     let candidate;
     $.ajax({
@@ -84,6 +120,8 @@ $(function(){
       })
     })
   })
+
+  // When user votes, update candidate2 vote count
   $(".vote2").click(function() {
     let candidate;
     $.ajax({
@@ -106,6 +144,8 @@ $(function(){
       })
     })
   })
+
+  // If user has voted before, disable vote button
   $(".candidateDetails").click(function(){
     $.ajax({
       url: "http://localhost:3000/voters/"+localStorage.getItem("id"),
